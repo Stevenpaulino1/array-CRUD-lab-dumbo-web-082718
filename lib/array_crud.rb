1e2c88c02398cac899f8190de722518685eabe49
Steven Paulino <stevenpaulino1@hotmail.com>
2018-08-06 18:00:55 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,18 @@
 def create_an_empty_array
-  
+empty_array = []
 end
 
 def create_an_array
-  
+cool_array = [1, 2, 3, 4]
 end
 
+
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << 5
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift
 end
 
 def remove_element_from_end_of_array(array)
